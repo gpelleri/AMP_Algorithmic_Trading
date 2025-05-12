1627f9942b875e8ab2d74c9e95070a1abece44c9
gpelleri <126279746+gpelleri@users.noreply.github.com>
2025-05-12 18:38:26 +0200

    add benchmark comparaison

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ from utils.data_handler import DataHandler
 
 
 if __name__ == "__main__":
-    data_handler = DataHandler(ticker='AAPL', start_date='2004-01-01', end_date='2020-01-01')
+    start_date = '2010-01-01'
+    end_date = '2017-01-01'
+    risk_free_rate = 0.02
+    data_handler = DataHandler(ticker='AAPL', start_date=start_date, end_date=end_date)
     data = data_handler.fetch_data()
 
+    # Load S&P 500 benchmark data
+    benchmark_data_handler = DataHandler("^GSPC", start_date, end_date)
+    benchmark_data = benchmark_data_handler.fetch_data()
+
     my_strategy = MomentumStrategy(period=14, overbought=70, oversold=30)
-    backtest = Backtest(data, my_strategy)
+    backtest = Backtest(data, my_strategy, benchmark_data, risk_free_rate=risk_free_rate)
     results = backtest.run()
     my_strategy.plot_signals(data)
     print(results)
